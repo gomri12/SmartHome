07137cafdf3d17c27608991bf5bc98d195d06ee6
omri glam <gomri12@gmail.com>
2016-06-17 15:31:58 +0300

    egg

---
+++ SmartHome/MyButton.swift
@@ -0,0 +1,22 @@
+//
+//  MyButton.swift
+//  SmartHome
+//
+//  Created by omri ios on 6/17/16.
+//  Copyright © 2016 omri ios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyButton: UIButton {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+
+        self.layer.borderWidth = 1
+        self.backgroundColor = UIColor(red: 22/256, green: 160/256, blue: 134/256, alpha: 1)
+        self.tintColor = UIColor.whiteColor()
+        
+    }
+}
--- SmartHome/ViewController.swift
+++ SmartHome/ViewController.swift
@@ -15,11 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var usernameTB: UITextField!
+    
+    @IBOutlet weak var passwordTB: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginBTN(sender: AnyObject) {
+    }
 
 }
 
