29c5cd0693d751cb942e926ef684f3fd3cbcf8af
omri glam <gomri12@gmail.com>
2016-07-14 18:26:06 +0300

    crazy push
    firebase + auth

--- SmartHome/DeviceListTableViewController.swift
+++ SmartHome/DeviceListTableViewController.swift
@@ -7,20 +7,32 @@
 //
 import Foundation
 import UIKit
+import Firebase
 
 class DeviceListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     // MARK: Properties
     @IBOutlet var deviceTableView: UITableView!
-    var items: [String] = ["We", "Heart", "Swift"]
+    var items: [Device] = [Device(location: "",name: "Bedroom TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Livingroom TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Kitchen TV",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Front Lights",icon: UIImage(named: "airconditioner_icon")!),
+                           Device(location: "",name: "Bedroom Air Conditioner",icon: UIImage(named: "airconditioner_icon")!)]
+    
+    @IBAction func logoutBTN(sender: AnyObject) {
+        self.performSegueWithIdentifier("login", sender: nil)
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.items.count
     }
     
+    @IBAction func addDeviceBTN(sender: AnyObject) {
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:DeviceTableCell = self.deviceTableView.dequeueReusableCellWithIdentifier("cell") as! DeviceTableCell
         
-        cell.deviceLabel.text = self.items[indexPath.row]
+        cell.deviceLabel.text = self.items[indexPath.row].name
+        cell.deviceImage.image = self.items[indexPath.row].icon
         
         return cell
     }
--- SmartHome/LoginViewController.swift
+++ SmartHome/LoginViewController.swift
@@ -8,24 +8,52 @@
 
 import UIKit
 import Alamofire
+import Firebase
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        navigationItem.hidesBackButton = true
+        
     }
 
     @IBOutlet weak var usernameTB: UITextField!
     
     @IBOutlet weak var passwordTB: UITextField!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func loginBTN(sender: AnyObject) {
-        
+        FIRAuth.auth()?.createUserWithEmail(usernameTB.text!, password: passwordTB.text!, completion: {
+            user, error in
+            if error != nil{
+                self.login();
+            }
+            else{
+                print("user created")
+                self.login();
+            }
+        })
+    }
+    
+    func login(){
+        FIRAuth.auth()?.signInWithEmail(usernameTB.text!, password: passwordTB.text!, completion: {
+            user, error in
+            
+            if error != nil{
+                print("something wrong")
+            }
+            else{
+                print("user is logged")
+            }
+        })
     }
 
 }
